22b18ca1e24cffa609e0f6d763c5dc987afe0742
setare-sabouri <100029656+setare-sabouri@users.noreply.github.com>
2022-12-11 15:55:27 +0100

    first commit

---
+++ api.js
@@ -0,0 +1,17 @@
+
+async function getAllFruits() {
+    const res = await fetch("https://rickandmortyapi.com/api/character");
+    const data = await res.json();
+    console.log(data.results[0].name);
+
+    for (let i = 0; i < 10; i++) {
+        let imgEl = document.createElement("img");
+        imgEl.src=data.results[i].image;
+        document.body.appendChild(imgEl);
+        imgEl.after(data.results[i].name);
+
+        
+    }
+}
+
+getAllFruits();
